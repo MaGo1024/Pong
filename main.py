8092225a7c4bf130c99b823b7dd085f1557ce1d9
MaGo1024 <olumideodusote@gmail.com>
2026-02-11 01:07:52 +0000

    Created ball class and made it move.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
+
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +12,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball((0,0))
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -19,6 +23,10 @@ screen.onkey(l_paddle.go_down, "w")
 game_on = True
 
 while game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # if ball.ycor() > 300 or ball.ycor() < -300:
 
 screen.exitonclick()
